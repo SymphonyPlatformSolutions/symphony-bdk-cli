453f6990d7c1580705ccec6b93fe798ce63da059
Gabriel Luiz <gabriel.luiz@unesp.br>
2019-09-19 09:58:33 -0300

    Update authentication method on github

--- src/git/clone.js
+++ src/git/clone.js
@@ -1,33 +1,38 @@
 'use strict';
+const os = require('os');
 var nodegit = require('nodegit'),
   path = require('path');
 
-var url = "https://github.com/SymphonyPlatformSolutions/sms-dev-fe-template-app.git",
-  local = "~/Git/cli/sms-sdk-cli/src/tmp",
-  usernName = "gluizbr";
+var url = "git@github.com:SymphonyPlatformSolutions/sms-sdk-cli.git",
+  local = "src/tmp",
+  userName = "git";
+
+var pubPath = path.join(os.homedir(), 'Git/cli/sms-sdk-cli/src/git/', 'id_rsa.pub');
+var privPath = path.join(os.homedir(), 'Git/cli/sms-sdk-cli/src/git/', 'id_rsa');
+
+var cred = nodegit.Cred.sshKeyNew(
+  userName,
+  pubPath,
+  privPath,
+  'Symphony!123456');
 
 var cloneOpts = {
   fetchOpts: {
     callbacks: {
-      credentials: function (url, userName) {
-        return nodegit.Cred.sshKeyNew(
-          userName,
-          '~/Git/cli/sms-sdk-cli/src/git/id_rsa.pub',
-          '~/Git/cli/sms-sdk-cli/src/git/id_rsa',
-          'Symphony!123456');
+      credentials: function () {
+        return cred;
       }
     }
   }
 };
 
 export const getTemplateProject = () => {
+  console.log(pubPath, privPath);
   // Clone a given repository into the `./tmp` folder.
-  console.log('time to clone');
+  console.log('time to clone', cloneOpts.fetchOpts.callbacks.credentials());
   nodegit.Clone(url, local, cloneOpts).then(function (repo) {
     console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
   }).catch(function (err) {
     console.log(err);
   });
 };
-
-//"https://5230c7e25166988b6439129b0eeabcf629a70568@github.com/SymphonyPlatformSolutions/sms-dev-fe-template-app.git"
