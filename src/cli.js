b46ee134a0eb7b73b469be19a9c7a4d5408002d4
gluizbr <bielraawr@gmail.com>
2019-09-19 17:27:55 -0300

    Delete .tmp folder before and after CLI run

--- src/cli.js
+++ src/cli.js
@@ -2,11 +2,15 @@ import { getCommands } from "./commands/commands";
 import { getVariableValues } from "./questions/questions";
 import {gitFlow} from "./git/git";
 import {createExtensionApp} from "./files/copy";
+import {deleteFolderRecursive} from "./files/utils";
+import {local} from "../utils/constants";
 
 export async function cli(args) {
   let options = getCommands(args);
   options = await getVariableValues(options);
   console.log(options);
+  deleteFolderRecursive(local);
   await gitFlow();
   await createExtensionApp(options);
+  deleteFolderRecursive(local);
 }
--- src/git/git.js
+++ src/git/git.js
@@ -1,10 +1,7 @@
 import {getTemplateProject} from "./process/clone";
 import {checkoutBranch} from "./process/checkout";
-import {deleteFolderRecursive} from "../files/utils";
-import {local} from "../../utils/constants";
 
 export async function gitFlow() {
-  deleteFolderRecursive(local);
   await getTemplateProject();
   await checkoutBranch();
 }
