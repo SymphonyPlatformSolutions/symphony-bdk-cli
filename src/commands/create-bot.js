c04a8507490e3d3c0cae11efbebf1ba81795004c
Lucas Rovaris <lucas.rovaris@devfactory.com>
2019-10-07 13:44:19 -0400

    feat(bot): cli branch name

--- src/commands/create-bot.js
+++ src/commands/create-bot.js
@@ -9,7 +9,7 @@ import {botFilesFlow} from "../files/files";
 import fs from 'fs';
 
 const repoUrl = "git@github.com:SymphonyPlatformSolutions/sms-sdk-botler.git";
-const repoBranch = 'refs/remotes/origin/songwriter-java11';
+const repoBranch = 'refs/remotes/origin/songwriter';
 
 const createBot = async (options) => {
   console.log(chalk.bold('Setting up a new Bot application'));
