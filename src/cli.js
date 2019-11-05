3cfd1659b1e36c3f2299c053eb47a4761108e816
rovaris <lrovaris@daitan.com>
2019-11-05 20:19:02 -0300

    feat(img): trying to get a better definition out of the image

--- src/cli.js
+++ src/cli.js
@@ -5,34 +5,36 @@ import generateKeys from "./commands/generate-keys";
 import checkDeps from "./commands/check-deps";
 import path from 'path';
 import launchToolbox from "./commands/launch-toolbox";
+import fs from 'fs';
 
-const terminalImage = require('terminal-image');
+const terminalImage = require('terminal-image-resizeable');
 
 const init = async (args) => {
   let options = getCommands(args);
+  const imageBuffer = fs.readFileSync(path.resolve(__dirname,'./assets/logo-symphony.png'));
 
   if (!checkDeps(options)) {
     return;
   }
   switch (options.command) {
     case COMMANDS.CREATE_EXT_APP:
-      console.log(await terminalImage.file(path.resolve(__dirname, './assets/logo-symphony.png')));
+      console.log(await terminalImage.buffer(imageBuffer, 64,30));
       createExtensionApp(options);
       break;
     case COMMANDS.CREATE_BOT:
-      console.log(await terminalImage.file(path.resolve(__dirname, './assets/logo-symphony.png')));
+      console.log(await terminalImage.buffer(imageBuffer, 64, 30));
       createBot(options);
       break;
     case COMMANDS.CREATE_CERTIFICATE:
-      console.log(await terminalImage.file(path.resolve(__dirname, './assets/logo-symphony.png')));
+      console.log(await terminalImage.buffer(imageBuffer, 64, 30));
       generateKeys(options);
       break;
       case COMMANDS.CHECK_DEPS:
-      console.log(await terminalImage.file(path.resolve(__dirname, './assets/logo-symphony.png')));
+      console.log(await terminalImage.buffer(imageBuffer, 64, 30));
       checkDeps(options);
       break;
     case COMMANDS.TOOLBOX:
-      console.log(await terminalImage.file(path.resolve(__dirname, './assets/logo-symphony.png')));
+      console.log(await terminalImage.buffer(imageBuffer, 64, 30));
       checkDeps(options);
       launchToolbox();
       break;
